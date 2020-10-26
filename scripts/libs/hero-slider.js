124429505927931185acb6a9c8f5db32b05add9e
junpeikiseki <71343624+junpeikiseki@users.noreply.github.com>
2020-10-27 02:33:28 +0900

    Add files via upload

---
+++ scripts/libs/hero-slider.js
@@ -0,0 +1,40 @@
+class HeroSlider {
+  constructor(el) {
+    this.el = el
+    this.swiper = this._initSwiper()
+  }
+
+  _initSwiper() {
+    return new Swiper(this.el, {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+      grabCursor: true,
+      effect: 'coverflow',
+      centeredSlides: true,
+      slidesPerView: 1,
+      speed: 2500,
+      breakpoints: {
+        1024: {
+          slidesPerView: 1,
+        },
+      },
+    })
+  }
+
+  start(options = {}) {
+    options = Object.assign(
+      {
+        delay: 6500,
+        disableOnInteraction: false,
+      },
+      options,
+    )
+
+    this.swiper.params.autoplay = options
+    this.swiper.autoplay.start()
+  }
+  stop() {
+    this.swiper.autoplay.stop()
+  }
+}
